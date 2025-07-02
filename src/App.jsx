5d387ababcaefafc24f83179d1a302dfcc75b2a4
Mikhail Myshakin <myshakinm@mail.ru>
2025-07-02 14:52:30 +0300

    add "src" in paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,36 @@ import { useEffect, useRef, useState } from "react";
 import "./fonts.css";
 import "./styles.css";
 
-export function Footer() {
-    return (
-        <footer class="footer">
-            <ul class="footer__list">
-                <li class="footer__item">
-                    <a class="footer__link" href="/">
-                        Помощь
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a class="footer__link" href="/">
-                        Обратная связь
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a class="footer__link" href="/">
-                        Разработчикам
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a class="footer__link" href="/">
-                        Условия использования
-                    </a>
-                </li>
-            </ul>
+// export function Footer() {
+//     return (
+//         <footer class="footer">
+//             <ul class="footer__list">
+//                 <li class="footer__item">
+//                     <a class="footer__link" href="/">
+//                         Помощь
+//                     </a>
+//                 </li>
+//                 <li class="footer__item">
+//                     <a class="footer__link" href="/">
+//                         Обратная связь
+//                     </a>
+//                 </li>
+//                 <li class="footer__item">
+//                     <a class="footer__link" href="/">
+//                         Разработчикам
+//                     </a>
+//                 </li>
+//                 <li class="footer__item">
+//                     <a class="footer__link" href="/">
+//                         Условия использования
+//                     </a>
+//                 </li>
+//             </ul>
 
-            <div class="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
-        </footer>
-    );
-}
+//             <div class="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
+//         </footer>
+//     );
+// }
 export function Header() {
     let [expanded, setExpanded] = useState(false);
     let [toggled, setToggled] = useState(false);
